c30c10e301bbb03fe3f4c2ab54f6c68eead770e6
Iván L. Murillo <diskman199@gmail.com>
2021-01-07 22:18:05 -0600

    Cambios mínimos
    Visual y de funcionalidad

--- app/src/main/java/com/dev/posystem/MonthlyUtilities.java
+++ app/src/main/java/com/dev/posystem/MonthlyUtilities.java
@@ -107,8 +107,7 @@ public class MonthlyUtilities extends AppCompatActivity implements DatePickerDia
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                finish();
             }
         });
 
